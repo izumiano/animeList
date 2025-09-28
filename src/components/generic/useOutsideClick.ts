5afcb2f200352ce192954bb15196d035d7a438ee
izumiano <izumiano@users.noreply.github.com>
2025-09-28 18:22:39 +0200

    fix not all dropdowns closing

--- src/components/generic/useOutsideClick.ts
+++ src/components/generic/useOutsideClick.ts
@@ -1,23 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const useOutsideClick = <T extends HTMLElement>(
   callback: () => void
 ) => {
-  const ref = useRef<T>(null); // Replace HTMLDivElement with the appropriate element type
+  const ref = useRef<T>(null);
+  const [isAdded, setIsAdded] = useState(false);
 
   useEffect(() => {
+    if (isAdded) return;
+
     const handleClickOutside = (event: MouseEvent | TouchEvent) => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
-        console.log("close");
         callback();
       }
     };
 
     document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, [callback]);
+    setIsAdded(true);
+  }, [callback, isAdded]);
 
   return ref;
 };
