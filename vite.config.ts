c65685274559ca55e6b60a5ab5461517e9058849
izumiano <izumiano@users.noreply.github.com>
2025-10-03 14:28:11 +0200

    try cloudflare pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/animeList/",
+  base: "/",
   resolve: {
     alias: {
       csstype: "csstype/index.d.ts",
