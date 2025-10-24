783daa067f7811d675ba3e2fcebe66ea7547d36a
izumiano <izumiano@users.noreply.github.com>
2025-10-24 10:41:45 +0200

    rename animeImport.ts -> animeImport.tsx and better error handling

---
+++ src/components/addAnimeMenu/animeImport.tsx
@@ -0,0 +1,99 @@
+import LocalDB from "../../indexedDb/indexedDb";
+import Anime from "../../models/anime";
+import { showError } from "../../utils/utils";
+
+export function importAnimes(
+	files: FileList | null,
+	onAnimeSaved: (anime: Anime) => void,
+) {
+	if (!files) {
+		return;
+	}
+
+	const animes: any[] = [];
+
+	new Promise((resolve, reject) => {
+		(async () => {
+			for (const file of files) {
+				try {
+					const obj = JSON.parse(await file.text());
+					for (const o of obj) {
+						animes.push(o);
+					}
+				} catch (ex) {
+					reject(ex);
+					return;
+				}
+			}
+
+			animes.sort((lhs, rhs) => {
+				if (lhs.order < rhs.order) return -1;
+				return 1;
+			});
+
+			LocalDB.doTransaction(
+				(store, db) => {
+					animes.forEach((anime, index) => {
+						anime.order = index;
+
+						if (typeof anime.dateStarted === "number") {
+							anime.dateStarted = thirdMilleniumSecondsToUnixMilli(
+								anime.dateStarted,
+							);
+						}
+						if (typeof anime.dateStarted === "number") {
+							anime.dateFinished = thirdMilleniumSecondsToUnixMilli(
+								anime.dateFinished,
+							);
+						}
+
+						anime.seasons.forEach((season: any) => {
+							if (typeof season.dateStarted === "number") {
+								season.dateStarted = thirdMilleniumSecondsToUnixMilli(
+									season.dateStarted,
+								);
+							}
+							if (typeof anime.dateStarted === "number") {
+								season.dateFinished = thirdMilleniumSecondsToUnixMilli(
+									season.dateFinished,
+								);
+							}
+							if (season.externalLink.type === "TMDB") {
+								season.externalLink.seasonId = season.externalLink?.id;
+								season.externalLink.id = anime.externalLink?.id;
+							}
+						});
+						const newAnime = Anime.Load({
+							animeData: anime,
+							justAdded: false,
+						});
+						db.saveAnime(newAnime, store).onsuccess = () => {
+							onAnimeSaved(newAnime);
+						};
+					});
+
+					return null;
+				},
+				{ onError: reject },
+			);
+			resolve(null);
+		})();
+	}).catch((ex) => {
+		showError(
+			ex,
+			<span>
+				<b>Failed importing data</b>
+			</span>,
+		);
+	});
+}
+
+function thirdMilleniumSecondsToUnixMilli(
+	thirdMilleniumTimestamp: number | undefined | null,
+) {
+	if (!thirdMilleniumTimestamp) {
+		return;
+	}
+
+	return thirdMilleniumTimestamp * 1000 + 978307200000;
+}
