c9c3a051e5a269f0061e3874a7e35b75047ca122
izumiano <izumiano@users.noreply.github.com>
2025-10-29 12:41:39 +0100

    env type safety

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_CLIENT_ID: string;
+	readonly VITE_EXTERNAL_SYNC_ENABLED: string;
+	readonly VITE_DEV_UTILS_ENABLED: string;
+	readonly VITE_USE_MOCK_DATA: string;
+	readonly VITE_FORCE_MOCK_DATA: string;
+	readonly VITE_DELETE_ALL: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
