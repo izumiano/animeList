dff37464d6f4c96ba3284c098b08d166b9340355
izumiano <izumiano@users.noreply.github.com>
2025-11-11 23:44:03 +0100

    login button style

--- src/components/accountNode.tsx
+++ src/components/accountNode.tsx
@@ -213,6 +213,7 @@ export default function AccountNode() {
 												onClick={() => {
 													externalLinkData.authInstance.login();
 												}}
+												className="listIgnore transparentBackground"
 											>
 												Login
 											</button>
